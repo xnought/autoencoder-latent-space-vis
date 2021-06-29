37683ad7d7acb58009834bc9b9e420880f591e3d
xnought <bertuccd@oregonstate.edu>
2021-06-29 15:39:42 -0700

    added the share buttons

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -143,15 +143,37 @@
 	{:else}
 		<h1>Loading MNIST Digits test and Models...</h1>
 	{/if}
-	<a
-		href="https://colab.research.google.com/drive/1jy6zd6mxneepQZe_UiHsz6CMn6mj3A-e?usp=sharing"
-		target="_blank"
-		id="colab"
-		><img
-			src="https://colab.research.google.com/assets/colab-badge.svg"
-			alt="Open In Colab"
-		/></a
-	>
+	<div id="button-container">
+		<div>
+			<a
+				href="https://github.com/xnought/autoencoder-latent-space-vis"
+				target="_blank"
+				id="github"
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					><path
+						d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+					/></svg
+				></a
+			>
+		</div>
+		<div id="divider" />
+		<div>
+			<a
+				href="https://colab.research.google.com/drive/1jy6zd6mxneepQZe_UiHsz6CMn6mj3A-e?usp=sharing"
+				target="_blank"
+				id="colab"
+				><img
+					src="https://colab.research.google.com/assets/colab-badge.svg"
+					alt="Open In Colab"
+				/></a
+			>
+		</div>
+	</div>
 </main>
 
 <style lang="scss">
@@ -163,9 +185,20 @@
 	// 		color: white;
 	// 	}
 	// }
-	#colab {
+	#button-container {
 		position: fixed;
-		bottom: 25px;
-		right: 25px;
+		right: 20px;
+		bottom: 20px;
+
+		display: flex;
+		align-items: center;
+	}
+
+	#divider {
+		background: grey;
+		height: 40px;
+		width: 1px;
+		margin-right: 10px;
+		margin-left: 10px;
 	}
 </style>
--- client/src/main.ts
+++ client/src/main.ts
@@ -3,7 +3,7 @@ import App from "./App.svelte";
 const app = new App({
 	target: document.body,
 	props: {
-		title: "Autoencoder Lab",
+		title: "Explore Latent Space",
 	},
 });
 
